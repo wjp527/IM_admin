feb5bb1985f6a3e99ef60c76f091f9c46bd34854
wjp <1060638479@qq.com>
2024-09-20 20:58:09 +0800

    完成netty和websocket的搭建

--- src/main/java/com/example/videocall/VideoCallApplication.java
+++ src/main/java/com/example/videocall/VideoCallApplication.java
@@ -1,13 +1,26 @@
 package com.example.videocall;
 
+import com.example.videocall.netty.CoordinationNettyServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VideoCallApplication {
+// 启动类
+public class VideoCallApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(VideoCallApplication.class, args);
     }
 
+    // 注入Netty服务
+    @Autowired
+    private CoordinationNettyServer nettyServer;
+
+    // 启动Netty服务
+    @Override
+    public void run(String... args) throws Exception {
+        nettyServer.start();
+    }
 }
---
+++ src/main/java/com/example/videocall/controller/Test.java
@@ -0,0 +1,11 @@
+package com.example.videocall.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Test {
+
+}
