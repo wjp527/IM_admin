81ea34f7d900458383adc417814c64b92396ce8d
wjp <1060638479@qq.com>
2024-09-20 20:37:50 +0800

    即时通信 接口项目初始化

---
+++ src/main/java/com/example/videocall/VideoCallApplication.java
@@ -0,0 +1,13 @@
+package com.example.videocall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoCallApplication.class, args);
+    }
+
+}
